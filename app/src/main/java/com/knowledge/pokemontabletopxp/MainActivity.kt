8b8cfa3fbf8db6f966076d37bc6373c757fefebf
Luan <luanspinheiro@gmail.com>
2021-04-16 18:08:12 -0300

    Insere as opções do spinner para aplicar o nível de comando.

--- app/src/main/java/com/knowledge/pokemontabletopxp/MainActivity.kt
+++ app/src/main/java/com/knowledge/pokemontabletopxp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.knowledge.pokemontabletopxp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
+        val spinner: Spinner = findViewById(R.id.spinner_id)
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.commandlevel,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
     }
+
+
 }
