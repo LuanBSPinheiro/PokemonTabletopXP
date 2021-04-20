0bd6671187eb6571672f02c273d0022061306c6b
Luan <luanspinheiro@gmail.com>
2021-04-19 22:55:46 -0300

    Insere regra de negócio para cálculo de bonus de XP e nível do Pokémon.

--- app/src/main/java/com/knowledge/pokemontabletopxp/MainActivity.kt
+++ app/src/main/java/com/knowledge/pokemontabletopxp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.knowledge.pokemontabletopxp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
+        val totalBonusView = findViewById<TextView>(R.id.totalbonus)
+        val commandList = resources.getStringArray(R.array.commandlevel)
         val spinner: Spinner = findViewById(R.id.spinner_id)
+        val checkBoxView: CheckBox = findViewById(R.id.checkBox)
+        var commandLevelSelected = 0
+        var totalBonusValue = 0
+        val pokemonXpView = findViewById<AppCompatEditText>(R.id.actualxp)
+        val pokemonLevelView = findViewById<TextView>(R.id.pkmnValue)
+
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
             this,
@@ -25,8 +37,51 @@ class MainActivity : AppCompatActivity() {
             spinner.adapter = adapter
         }
 
-        val lv = GetPokemonLevel.getLv(15)
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                println("Nothing")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                commandLevelSelected = commandList[position].toInt()
+                totalBonusValue = getTotalBonus(commandLevelSelected, checkBoxView.isChecked)
+                totalBonusView.text = totalBonusValue.toString()
+            }
+
+        }
+
+        checkBoxView.setOnCheckedChangeListener { _, isChecked ->
+            totalBonusValue = getTotalBonus(commandLevelSelected, isChecked)
+            totalBonusView.text = totalBonusValue.toString()
+        }
+
+        pokemonXpView.doAfterTextChanged {
+            val lvl = it.toString().toInt()
+            pokemonLevelView.text = GetPokemonLevel.getLv(lvl).toString()
+        }
+
+
+        
     }
 
+    fun getTotalBonus(commandLevel: Int, isChecked: Boolean) : Int {
+
+        val bonus : Int = if (isChecked && commandLevel >= 5) 5 else 0
+
+        return when {
+            commandLevel <= 2 -> {
+                bonus + 0
+            }
+            commandLevel <= 4 -> {
+                bonus + 5
+            }
+            commandLevel <= 6 -> {
+                bonus + 10
+            }
+            else -> {
+                bonus + 15
+            }
+        }
+    }
 
 }
