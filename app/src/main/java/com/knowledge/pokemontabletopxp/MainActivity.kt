1c3ad757399e4694882efa94da995c3ec7007de3
Luan <luanspinheiro@gmail.com>
2021-04-20 01:00:02 -0300

    Insere função do botão de executar.
    Bug conhecido - Crash se o sistema operacional estiver com o modo noturno ativado.

--- app/src/main/java/com/knowledge/pokemontabletopxp/MainActivity.kt
+++ app/src/main/java/com/knowledge/pokemontabletopxp/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.knowledge.pokemontabletopxp
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
-import androidx.core.widget.addTextChangedListener
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.widget.doAfterTextChanged
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+       // AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         val totalBonusView = findViewById<TextView>(R.id.totalbonus)
         val commandList = resources.getStringArray(R.array.commandlevel)
@@ -24,6 +24,13 @@ class MainActivity : AppCompatActivity() {
         var totalBonusValue = 0
         val pokemonXpView = findViewById<AppCompatEditText>(R.id.actualxp)
         val pokemonLevelView = findViewById<TextView>(R.id.pkmnValue)
+        val trainingNumberView = findViewById<AppCompatEditText>(R.id.trainingnumber)
+        val buttonCalcView = findViewById<AppCompatButton>(R.id.buttomcalc)
+        val pkmnFinalXpView = findViewById<AppCompatTextView>(R.id.pkmnFinalXp)
+        val pkmnFinalLvView = findViewById<AppCompatTextView>(R.id.pkmnFinalLv)
+        var totalFinalXp: Int
+        var training = 0
+        var lvl = 0
 
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
@@ -37,12 +44,17 @@ class MainActivity : AppCompatActivity() {
             spinner.adapter = adapter
         }
 
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 println("Nothing")
             }
 
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long,
+            ) {
                 commandLevelSelected = commandList[position].toInt()
                 totalBonusValue = getTotalBonus(commandLevelSelected, checkBoxView.isChecked)
                 totalBonusView.text = totalBonusValue.toString()
@@ -56,17 +68,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         pokemonXpView.doAfterTextChanged {
-            val lvl = it.toString().toInt()
-            pokemonLevelView.text = GetPokemonLevel.getLv(lvl).toString()
+            val pkmnXp = it.toString().toInt()
+            lvl = GetPokemonLevel.getLv(pkmnXp)
+            pokemonLevelView.text = lvl.toString()
         }
 
+        trainingNumberView.doAfterTextChanged {
+            training = it.toString().toInt()
+        }
 
-        
+        buttonCalcView.setOnClickListener {
+            totalFinalXp =
+                (getCalc(totalBonusValue, lvl, training) + pokemonXpView.text.toString().toInt())
+            pkmnFinalXpView.text = totalFinalXp.toString()
+            pkmnFinalLvView.text = GetPokemonLevel.getLv(totalFinalXp).toString()
+        }
+
     }
 
-    fun getTotalBonus(commandLevel: Int, isChecked: Boolean) : Int {
+    fun getTotalBonus(commandLevel: Int, isChecked: Boolean): Int {
 
-        val bonus : Int = if (isChecked && commandLevel >= 5) 5 else 0
+        val bonus: Int = if (isChecked && commandLevel >= 5) 5 else 0
 
         return when {
             commandLevel <= 2 -> {
@@ -82,6 +104,12 @@ class MainActivity : AppCompatActivity() {
                 bonus + 15
             }
         }
+    }
+
+    private fun getCalc(getTotalBonus: Int, lvl: Int, training: Int): Int {
+
+        return training * (lvl / 2 + getTotalBonus)
+
     }
 
 }
